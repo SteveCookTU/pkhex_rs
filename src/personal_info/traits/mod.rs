1e4740b0d4decc1a06c3d1a503a9afdeaf6bf594
Steve Cook <steviecook210@gmail.com>
2022-12-11 13:49:39 -0500

    PersonalMisc trait

--- src/personal_info/traits/mod.rs
+++ src/personal_info/traits/mod.rs
@@ -5,6 +5,7 @@ mod personal_ability;
 mod personal_egg;
 mod personal_encounter;
 mod personal_form_info;
+mod personal_misc;
 
 pub use base_state::*;
 pub use effort_value_yield::*;
@@ -13,3 +14,4 @@ pub use personal_ability::*;
 pub use personal_egg::*;
 pub use personal_encounter::*;
 pub use personal_form_info::*;
+pub use personal_misc::*;
